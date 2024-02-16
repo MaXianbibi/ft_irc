bd1038f234e4c14f4dbab19d994c9469132020a8
Justin Morneau <jmorneau25@gmail.com>
2024-02-16 12:11:28 -0500

    Add command line argument validation

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char **argv) {
     Server server;
 
+   #ifdef BUILD 
     if (argc != 3) {std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl; exit(1);}
 
     int port = atoi(argv[1]);
@@ -13,9 +14,10 @@ int main(int argc, char **argv) {
     if (key.size() < 1) { std::cerr << "Invalid password" << std::endl; exit(1);}
     if (key.size() > 50) { std::cerr << "Password too long" << std::endl; exit(1);}
     server.set_keypass(key);
+    #endif
 
     server.selectInit();
     server.selectLoop();
-    
+
     return 0;
 }
